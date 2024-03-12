4d5ed80546fbde6453b8d0691745b2f15da4daa5
Gustavo Martin <gustavomartin94@hotmail.com>
2024-03-11 22:02:32 -0300

    Actualizacion

---
+++ Creación Tabla clima.sql
@@ -0,0 +1,12 @@
+CREATE TABLE clima (
+    Latitud DECIMAL(9,6),
+    Longitud DECIMAL(9,6),
+    Fecha_pronost TIMESTAMP WITH TIME zone,
+    Ciudad VARCHAR(100),
+    Temperatura_C FLOAT,
+    Precipitacion FLOAT,
+    Velocidad_de_viento FLOAT,
+    Fecha_Actualizacion TIMESTAMP
+    
+);
+
