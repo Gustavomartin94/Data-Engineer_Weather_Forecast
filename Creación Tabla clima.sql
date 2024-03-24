e3eafa6bc0b5bd6033a8d5748b06ca3b5b305ade
Gustavomartin94 <122500501+Gustavomartin94@users.noreply.github.com>
2024-03-24 19:23:27 -0300

    Mensaje descriptivo de tu commit

--- Creación Tabla clima.sql
+++ Creación Tabla clima.sql
@@ -1,4 +1,5 @@
 CREATE TABLE clima (
+    Indice INT,
     Latitud DECIMAL(9,6),
     Longitud DECIMAL(9,6),
     Fecha_pronost TIMESTAMP WITH TIME zone,
@@ -7,7 +8,8 @@ CREATE TABLE clima (
     Humedad_porcentaje FLOAT,
     Velocidad_de_viento FLOAT,
     Fecha_Actualizacion TIMESTAMP,
-    Primary_Key VARCHAR(100)
-    
+    Primary_Key VARCHAR(100),
+    PRIMARY KEY (Primary_Key)
 );
+
 
